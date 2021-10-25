9a1414e33ed40c0c39af36df73c9f3b77391371e
PierreRlld <p.rouillard@outlook.com>
2021-10-26 00:21:20 +0200

    Added Dataframes for each considered players

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 
-#usernames
-pl=["PIERR0 LA RAFALE", "Nobusuke", "TITO LA TORPILLE", "chtis sournois", "CharLoRamBo"]
+pl=["PIERR0 LA RAFALE", "Nobusuke", "TITO LA TORPILLE", "chtis sournois", "CharLoRamBo"]    #usernames
 
 def modif(charac):
     return charac.replace(" ","%20")
-
-#list of players for opgg
-plsearch=list(map(modif,pl))
+plsearch=list(map(modif,pl))    #list of players for opgg
 
 import pandas as pd
+
+var=['Champion','Result','CS','CSmin','Kills','Deaths','Assists','KDA']
+dP=pd.DataFrame(index=[0],columns=var)
+dG=dP.copy()
+dT=dP.copy()
+dC=dP.copy()
+dGr=dP.copy()
+
+print(dP)
