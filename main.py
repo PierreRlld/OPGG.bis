9a0fd93f4eff7dcbefc20b97dd58c2e310567763
PierreRlld <p.rouillard@outlook.com>
2021-10-26 14:03:08 +0200

    commit changes

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ plsearch=list(map(modif,pl))    #list of players for opgg
 
 import pandas as pd
 
-var=['Champion','Result','CS','CSmin','Kills','Deaths','Assists','KDA']
+#GameId=timekdachamp
+var=['Champion','Result','CS','CSmin','Kills','Deaths','Assists','KDA','GameID']
 dP=pd.DataFrame(index=[0],columns=var)
 dG=dP.copy()
 dT=dP.copy()
 dC=dP.copy()
 dGr=dP.copy()
 
-print(dP)
+#print(dP)
