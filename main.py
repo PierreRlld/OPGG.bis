a8006721469cac1ec03674da198eb52ac3e238cf
PierreRlld <p.rouillard@outlook.com>
2021-10-26 17:33:24 +0200

    first commit of SetUpdate function

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
+import pandas as pd
+from datacollectorv2 import updategames, modif
 
 pl=["PIERR0 LA RAFALE", "Nobusuke", "TITO LA TORPILLE", "chtis sournois", "CharLoRamBo"]    #usernames
-
-def modif(charac):
-    return charac.replace(" ","%20")
-plsearch=list(map(modif,pl))    #list of players for opgg
-
-import pandas as pd
-
-#GameId=timekdachamp
-var=['Champion','Result','CS','CSmin','Kills','Deaths','Assists','KDA','GameID']
+var=['Champion','Result','CS','CSmin','Kills','Deaths','Assists','KDA','GameID']        #GameId=time/kda@champ
+# Initializing dataframes =============
 dP=pd.DataFrame(index=[0],columns=var)
 dG=dP.copy()
 dT=dP.copy()
-dC=dP.copy()
 dGr=dP.copy()
+dC=dP.copy()
 
-#print(dP)
+Set={"PIERR0 LA RAFALE":dP,"Nobusuke":dG,"TITO LA TORPILLE":dT,"chtis sournois":dGr,"CharLoRamBo":dC}
+
+
+"""
+def SetUpdate(username):
+    df=Set[username]    #selecting the dataframe corresponding to the username
+    up=updategames(username)
+    if up[0]==None:
+        return df 
+    else :
+        df.append(updategames(username),sort=False)
+        df.set_index(i for i in range(1,len(df)+1))
+        #delete games aldready in the dataset
+        indexnames=df[].index
+"""
+
+test=updategames('Nobusuke')
+testb=pd.DataFrame(Data=['Kennen', 'Lose', '279', 6.2, '5', '13', '2', '0.54', '45:33/0.54@Kennen'],index=[1],columns=var)
