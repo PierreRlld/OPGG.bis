f9af15d47c2d17d54cb307a95a64ae6daa36c8d6
PierreRlld <p.rouillard@outlook.com>
2021-10-26 13:00:08 +0200

    first try of listing all variables in L for each game

--- datacollectorv2.py
+++ datacollectorv2.py
@@ -1,11 +1,11 @@
-from main import pl, plsearch
+from main import pl, plsearch, var
 
 def fonc_url(username):
     return "https://euw.op.gg/summoner/userName="+username
 #print(fonc_url(plsearch[0]))
 
 # URL SETUP :
-URL=fonc_url(plsearch[2])
+URL=fonc_url(plsearch[1])
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -26,22 +26,41 @@ try :
 except :
     pass
 
-"""
-# Updating profile but it doesn't seem to be needed
-profile=driver.find_element(By.CLASS_NAME, 'Profile')
-button=profile.find_element(By.CLASS_NAME, 'Buttons')
-button.click()
-"""
 
-game_list=driver.find_element(By.CLASS_NAME, 'GameItemList')    # "Block" of all games
+#Updating profile but it doesn't seem to be needed
+#profile=driver.find_element(By.CLASS_NAME, 'Profile')
+#button=profile.find_element(By.CLASS_NAME, 'Buttons')
+#button.click()
+
+""" More games """
+more=driver.find_element(By.CLASS_NAME, 'GameMoreButton Box')
+more.click()
+
+game_list=driver.find_element(By.CLASS_NAME, 'GameItemList')    # "Block" of all games > should be extented to maximise the number of games located
 games=game_list.find_elements(By.CLASS_NAME, 'GameItemWrap')    # Each game is now a selenium element
 
-
-champs=[]
+#var=['GameId','Champion','Result','CS','CSmin','Kills','Deaths','Assists','KDA']
+gametype='Normale'
+L=[]
 for game in games : #pb j'ai tous les types de game encore là
-    champion=game.find_element(By.CLASS_NAME, 'ChampionName')
-    champs.append(champion.text)
-print(champs)
+    lg=[]
+    if game.find_element(By.CLASS_NAME,'GameType').text==gametype :
+        lg.append(game.find_element(By.CLASS_NAME,'data-game-id').text)      #gameid
+        lg.append(game.find_element(By.CLASS_NAME, 'ChampionName').text)     #champion
+        lg.append(game.find_element(By.CLASS_NAME, 'GameResult').text)       #result
+        cs=game.find_element(By.CLASS_NAME, 'CS tip')               
+        lg.append(cs.text[0:3])     #CS
+        csm=cs.text[-2]
+        lg.append(csm)      #CSmin
+        lg.append(game.find_element(By.CLASS_NAME,'Kill').text)     #kills
+        lg.append(game.find_element(By.CLASS_NAME,'Death').text)    #deaths
+        lg.append(game.find_element(By.CLASS_NAME,'Assist').text)   #assists
+        kda=game.find_element(By.CLASS_NAME,'KDARatio')
+        lg.append(kda.text[0:len(kda.text)-2])      #KDA
+        L.append(lg)
+    else :
+        pass
+print(L)
 
 time.sleep(1) #sinon ferme dès que ça charge quand y'a rien après
 driver.quit()
